98e34707716d6e203e20d627029d181b52b8dd11
Emillyn Muniz <emillyn_muniz@hotmail.com>
2020-08-04 23:33:25 -0300

    Create Main.java

---
+++ Java/Framework/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpsemana11;
+
+
+public class TPSemana11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Livro umlivro = new Livro();
+
+        MyDAL.geraTabela(umlivro);
+    }
+    
+}
